5f2a7692c3a33f1d440cb0bfc19723a8afcbf63c
Sara Maldum <saramaldum@hotmail.com>
2020-08-17 09:39:34 +0200

    Added heading 2 to Portfolio.js

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Heading1 from '../layout/headings/Heading1';
-import projects from '../../constants/projects';
+import Heading2 from '../layout/headings/Heading2';
 import Heading3 from '../layout/headings/Heading3';
+import projects from '../../constants/projects';
 
 function Portfolio() {
     return (
         <>
             <Heading1>Portfolio</Heading1>
             <div>
+                <Heading2>School projects</Heading2>
                 {projects.map((project, index) => 
                     <div key={index} className="col-md-6">
                         <Heading3>{project.name}</Heading3>
